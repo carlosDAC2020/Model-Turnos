8d61ce5fde40a86bbea82d9413b12373aa82d716
Carlos Agamez <carlos.agamez@sigma3ds.com>
2024-09-29 13:38:45 -0500

    deploy

--- main.py
+++ main.py
@@ -122,4 +122,4 @@ def eliminar_cronograma(cronograma_id):
         return jsonify({'error': 'Cronograma no encontrado'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
